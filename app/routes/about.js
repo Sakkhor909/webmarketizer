00bed8a188936c6476175b41f7f928070f4cc957
Sakkhor909 <minhajur.rahman.sakkhor@gmail.com>
2021-12-28 19:53:36 +0600

    services page version 1 completed

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class AboutRoute extends Route {}
+export default class AboutRoute extends Route {
+  model() {
+    return ['hello', '2', 'about ?'];
+  }
+}
