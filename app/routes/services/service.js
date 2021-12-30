73355dd1441939bca616a055bd4ecd4296d45926
Sakkhor909 <minhajur.rahman.sakkhor@gmail.com>
2021-12-31 01:16:08 +0600

    Single service page template design complete

--- app/routes/services/service.js
+++ app/routes/services/service.js
@@ -1,8 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default class ServicesServiceRoute extends Route {
-  model(params) {
+  async model(params) {
     const { service_slug } = params;
-    return service_slug;
+
+    let response = await fetch(`/api/${service_slug}.json`).catch((error) => {
+      console.log(
+        `There is an error with single service data fetching ${error}`
+      );
+    });
+
+    let parsed = await response.json();
+
+    return parsed.data;
   }
 }
