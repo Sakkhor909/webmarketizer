59b5508eca7ff15aeea37b479ce0e8c7ff88d9cf
Sakkhor909 <minhajur.rahman.sakkhor@gmail.com>
2022-01-02 12:13:51 +0600

    images ready for dvelopment but not for production in netlify

---
+++ app/components/slider.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+
+export default class SliderComponent extends Component {
+  slideIndex = 0;
+
+  // Need to learn life cycle methood
+
+  // showSlides()
+
+  //  showSlides() {
+  //   var i;
+  //   var slides = document.getElementsByClassName("mySlides");
+  //   for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none";
+  //   }
+  //   slideIndex++;
+  //   if (slideIndex > slides.length) {slideIndex = 1}
+  //   slides[slideIndex-1].style.display = "block";
+  //   setTimeout(showSlides, 2000); // Change image every 2 seconds
+  // }
+}
